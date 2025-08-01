faf926cad9182615f068e9b173d4ea4c3407e2c8
Nathaniel Greve <103282285+nmgreve@users.noreply.github.com>
2025-07-31 20:26:43 -0400

    hopeful fix to upload issue

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,14 @@
-// firebase-config.js
+// Use CDN modules, not local node_modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "./node_modules/firebase/app/dist/index.esm.js";
-import { getAnalytics } from "./node_modules/firebase/analytics/dist/index.esm.js";
-import { getStorage } from "./node_modules/firebase/storage/dist/index.esm.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your config
 const firebaseConfig = {
   apiKey: "AIzaSyB1foDDpomvV7S_8ZpRtiUgIhYOp9Oi6H8",
   authDomain: "ncgreeting.firebaseapp.com",
   projectId: "ncgreeting",
-  storageBucket: "ncgreeting.appspot.com", // Corrected storageBucket URL
+  storageBucket: "ncgreeting.appspot.com",
   messagingSenderId: "947337187260",
   appId: "1:947337187260:web:1d512cc2415f68928565b9",
   measurementId: "G-HN5KMXN3RX"
@@ -23,9 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
 
-// Expose to script.js
-window.firebaseStorage = storage; // Now 'storage' is defined and refers to your Cloud Storage service!
+window.firebaseStorage = storage;
